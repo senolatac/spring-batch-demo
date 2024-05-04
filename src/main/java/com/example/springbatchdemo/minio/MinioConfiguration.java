a16ac580326658d15f889c19589e0520c44435a9
Şenol Ataç <senolatac@Senols-MBP.home>
2024-05-04 17:15:04 +0300

    minio

--- src/main/java/com/example/springbatchdemo/minio/MinioConfiguration.java
+++ src/main/java/com/example/springbatchdemo/minio/MinioConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -19,6 +20,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 @Slf4j
+@Lazy
 @Configuration
 @ConditionalOnClass(MinioClient.class)
 @EnableConfigurationProperties(MinioConfigurationProperties.class)
